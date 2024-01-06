7f4fd1f24863e04793f928d6aa166061ac594ce3
João Marcello Cavadinha F. Machado <93738205+J-cavadinha@users.noreply.github.com>
2024-01-06 20:35:46 -0300

    Update entradausuario.java

--- Básicos/entradausuario.java
+++ Básicos/entradausuario.java
@@ -10,10 +10,18 @@ public class entradausuario {
         System.out.print("Insira seu nome: ");
 
         // Recebe o input do usuário
-        String input1 = entrada.nextLine();
+        String nome = entrada.nextLine();
+
+        System.out.print("Insira sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.print("Insira seu salário: ");
+        float salario = entrada.nextFloat();
 
         // Exibe uma mensagem com o input do usuário
-        System.out.println("O seu nome é " + input1);
+        System.out.println("O seu nome é " + nome);
+        System.out.println("Você tem " + idade + " anos");
+        System.out.println("Você recebe por mês: " + salario);
 
         // Libera recursos
         entrada.close();
