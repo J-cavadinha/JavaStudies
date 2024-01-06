c52bd741d4d02a276cb44723fc76f724aaf345a2
João Marcello Cavadinha F. Machado <93738205+J-cavadinha@users.noreply.github.com>
2024-01-06 20:27:00 -0300

    Update entradausuario.java

--- Básicos/entradausuario.java
+++ Básicos/entradausuario.java
@@ -4,18 +4,18 @@ public class entradausuario {
     public static void main(String[] args) {
         // Realiza como se fosse uma declaração de função para simplificar o uso, estilo
         // typedef
-        Scanner scanner = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
         // Exibe mensagem pedindo input do usuário
         System.out.print("Insira seu nome: ");
 
         // Recebe o input do usuário
-        String input1 = scanner.nextLine();
+        String input1 = entrada.nextLine();
 
         // Exibe uma mensagem com o input do usuário
         System.out.println("O seu nome é " + input1);
 
         // Libera recursos
-        scanner.close();
+        entrada.close();
     }
 }
