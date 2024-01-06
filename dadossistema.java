f9b2d7ca3e76b6ebfbba13c938461f050f63363c
João Marcello Cavadinha F. Machado <93738205+J-cavadinha@users.noreply.github.com>
2024-01-06 18:38:57 -0300

    Primeiros códigos próprios na linguagem Java

---
+++ dadossistema.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.util.Locale;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+public class dadossistema {
+    public static void main(String[] args) {
+        Date data = new Date();
+        System.out.println("Data: " + data);
+
+        Locale idioma = Locale.getDefault();
+        System.out.println("Idioma: " + idioma.getDisplayLanguage());
+
+        Locale pais = Locale.getDefault();
+        System.out.println("País: " + pais.getDisplayCountry());
+
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        int largura = (int) size.getWidth();
+        int altura = (int) size.getHeight();
+        System.out.println("A resolução é: " + largura + " x " + altura);
+    }
+}
