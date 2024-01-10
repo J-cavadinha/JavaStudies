504059a6af3cd833838c5a86d0a874dc7480d480
João Marcello Cavadinha F. Machado <93738205+J-cavadinha@users.noreply.github.com>
2024-01-10 01:59:18 -0300

    Update mediaaluno.java

--- Básicos/mediaaluno.java
+++ Básicos/mediaaluno.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/* Atualizar futuramente com verificação do tipo de entrada das notas
+ * Implementar um meio de leitura com repetição, permitindo o usuário entrar
+ * com a quantidade de notas desejadas (for loop)
+ * Idéia de novo programa - fazer uma calculadora de médias contendo a média aritmética
+ * e a média ponderada.
+ */
+
 public class mediaaluno {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -8,26 +15,26 @@ public class mediaaluno {
         String nome_aluno = entrada.nextLine();
 
         System.out.print("Digite a sua 1a nota: ");
-        float nota1 = entrada.nextInt();
+        float nota1 = entrada.nextFloat();
 
         System.out.print("Digite a sua 2a nota: ");
-        float nota2 = entrada.nextInt();
+        float nota2 = entrada.nextFloat();
 
         System.out.print("Digite a sua 3a nota: ");
-        float nota3 = entrada.nextInt();
+        float nota3 = entrada.nextFloat();
         System.out.print("\n");
 
         float media = (nota1 + nota2 + nota3) / 3;
 
         if (media >= 7) {
             System.out.println("Parabéns, " + nome_aluno + " você foi aprovado!");
-        } else if (media > 5.9 && media < 7) {
+        } else if (media >= 6 && media < 7) {
             System.out.println("Você está de prova final " + nome_aluno + " acontece com todos :/");
         } else {
             System.out.println("Você foi reprovado :o");
         }
 
-        System.out.println("A sua média anual foi: " + media);
+        System.out.printf("A sua média anual foi: %.2f" + media);
 
         entrada.close();
     }
